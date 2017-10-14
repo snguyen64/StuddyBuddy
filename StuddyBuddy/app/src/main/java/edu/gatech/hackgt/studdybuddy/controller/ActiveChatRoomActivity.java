79a8bde77924d82c94825c591edc150d3f9dca75
Steven Nguyen <stevennguyen430@gmail.com>
2017-10-14 06:29:53 -0400

    edited live chat room and chatroom create activities

---
+++ StuddyBuddy/app/src/main/java/edu/gatech/hackgt/studdybuddy/controller/ActiveChatRoomActivity.java
@@ -0,0 +1,15 @@
+package edu.gatech.hackgt.studdybuddy.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.gatech.hackgt.studdybuddy.R;
+
+public class ActiveChatRoomActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_active_chat_room);
+    }
+}
