2ceac6a2403527152301f120655b73b3dcd14b4c
pace543 <sribhat31@gmail.com>
2017-10-13 23:34:47 -0400

    Added Course and User

---
+++ StuddyBuddy/app/src/main/java/edu/gatech/hackgt/studdybuddy/model/Course.java
@@ -0,0 +1,32 @@
+package edu.gatech.hackgt.studdybuddy.model;
+
+import com.google.gson.annotations.SerializedName;
+
+public class Course {
+    @SerializedName("courseType")
+    private CourseType courseType;
+
+    @SerializedName("courseNumber")
+    private int courseNumber;
+
+    public Course(CourseType courseType, int courseNumber) {
+        this.courseType = courseType;
+        this.courseNumber = courseNumber;
+    }
+
+    public CourseType getCourseType() {
+        return courseType;
+    }
+
+    public void setCourseType(CourseType courseType) {
+        this.courseType = courseType;
+    }
+
+    public int getCourseNumber() {
+        return courseNumber;
+    }
+
+    public void setCourseNumber(int courseNumber) {
+        this.courseNumber = courseNumber;
+    }
+}
