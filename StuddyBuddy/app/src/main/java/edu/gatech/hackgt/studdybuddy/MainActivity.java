85eed46c5625bc95f83c994db3fe4111de331ed0
Steven Nguyen <stevennguyen430@gmail.com>
2017-10-13 23:26:17 -0400

    added register and login activity

--- StuddyBuddy/app/src/main/java/edu/gatech/hackgt/studdybuddy/MainActivity.java
+++ StuddyBuddy/app/src/main/java/edu/gatech/hackgt/studdybuddy/MainActivity.java
@@ -1,13 +1,29 @@
 package edu.gatech.hackgt.studdybuddy;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    private Button login;
+    private Button register;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        login = (Button) findViewById(R.id.loginButton);
+        register = (Button) findViewById(R.id.registerButton);
+    }
+
+    public void login(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
+    }
+
+    public void register(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 }
