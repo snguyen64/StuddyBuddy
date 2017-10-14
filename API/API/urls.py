87b8c065014987327d892c66c6b0b25e2c6c6997
pace543 <sribhat31@gmail.com>
2017-10-14 03:01:49 -0400

    Added login and register to API

--- API/API/urls.py
+++ API/API/urls.py
@@ -15,7 +15,10 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from studdybuddy_api import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^auth/register/', views.register)
+
 ]
---
+++ API/studdybuddy_api/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class StuddybuddyApiConfig(AppConfig):
+    name = 'studdybuddy_api'
