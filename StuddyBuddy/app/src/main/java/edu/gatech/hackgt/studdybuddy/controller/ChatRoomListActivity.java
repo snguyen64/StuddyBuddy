913f999a0cc07038032fd1bdb9c205d12a015549
Steven Nguyen <stevennguyen430@gmail.com>
2017-10-14 04:26:04 -0400

    added activities

---
+++ StuddyBuddy/app/src/main/java/edu/gatech/hackgt/studdybuddy/controller/ChatRoomListActivity.java
@@ -0,0 +1,15 @@
+package edu.gatech.hackgt.studdybuddy.controller;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import edu.gatech.hackgt.studdybuddy.R;
+
+public class ChatRoomListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_chat_room_list);
+    }
+}
--- StuddyBuddy/app/src/main/java/edu/gatech/hackgt/studdybuddy/controller/ProfileCourseActivity.java
+++ StuddyBuddy/app/src/main/java/edu/gatech/hackgt/studdybuddy/controller/ProfileCourseActivity.java
@@ -1,15 +1,39 @@
 package edu.gatech.hackgt.studdybuddy.controller;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import edu.gatech.hackgt.studdybuddy.R;
 
 public class ProfileCourseActivity extends AppCompatActivity {
-
+    private Button findBuddies;
+    private Button addCourse;
+    private Button removeCourse;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile_course);
+        findBuddies = (Button) findViewById(R.id.findBuddiesButton);
+        addCourse = (Button) findViewById(R.id.addCourse);
+        removeCourse = (Button) findViewById(R.id.removeCourse);
     }
+    
+    public void findBuddies(View view) {
+        Intent intent = new Intent(this, ChatRoomListActivity.class);
+        startActivity(intent);
+    }
+
+    public void addCourse(View view) {
+        Intent intent = new Intent(this, CourseSelectionActivity.class);
+        startActivity(intent);
+    }
+
+    public void removeCourse(View view) {
+        Intent intent = new Intent(this, CourseSelectionActivity.class);
+        startActivity(intent);
+    }
+
 }
