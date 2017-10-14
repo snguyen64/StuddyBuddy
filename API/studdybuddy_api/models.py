186f4b4227899927b727e19575d43d434be7e8af
pace543 <sribhat31@gmail.com>
2017-10-14 04:55:08 -0400

    Added Course model to API

--- API/studdybuddy_api/models.py
+++ API/studdybuddy_api/models.py
@@ -1,3 +1,8 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+class Course(models.Model):
+    type = models.CharField(max_length=4)
+    number = models.IntegerField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
