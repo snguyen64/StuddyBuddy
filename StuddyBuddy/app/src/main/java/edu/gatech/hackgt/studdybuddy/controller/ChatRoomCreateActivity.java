2393a74f77c361aa35f80d86fae979e12ee3a0b1
Steven Nguyen <stevennguyen430@gmail.com>
2017-10-14 06:33:02 -0400

    modified chatroom activities

--- StuddyBuddy/app/src/main/java/edu/gatech/hackgt/studdybuddy/controller/ChatRoomCreateActivity.java
+++ StuddyBuddy/app/src/main/java/edu/gatech/hackgt/studdybuddy/controller/ChatRoomCreateActivity.java
@@ -3,6 +3,7 @@ package edu.gatech.hackgt.studdybuddy.controller;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -14,8 +15,10 @@ import android.widget.Toast;
 import java.util.List;
 
 import edu.gatech.hackgt.studdybuddy.R;
+import edu.gatech.hackgt.studdybuddy.model.Course;
 import edu.gatech.hackgt.studdybuddy.model.CourseType;
 import edu.gatech.hackgt.studdybuddy.util.APIClient;
+import edu.gatech.hackgt.studdybuddy.util.FormValidator;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Response;
@@ -24,7 +27,7 @@ public class ChatRoomCreateActivity extends AppCompatActivity {
 
     private EditText chatroomName;
     private Spinner courseType;
-    private Spinner course;
+    private Spinner courseSpinner;
     private Button submit;
     private Button cancel;
 
@@ -37,7 +40,7 @@ public class ChatRoomCreateActivity extends AppCompatActivity {
         ArrayAdapter<CourseType> courseTypeArrayAdapter = new ArrayAdapter<CourseType>(this,
                 android.R.layout.simple_spinner_item, CourseType.values());
         courseType.setAdapter(courseTypeArrayAdapter);
-        course = (Spinner) findViewById(R.id.course);
+        courseSpinner = (Spinner) findViewById(R.id.course);
         courseType.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
@@ -48,7 +51,7 @@ public class ChatRoomCreateActivity extends AppCompatActivity {
                     public void onResponse(Call<List<Integer>> call, Response<List<Integer>> response) {
                         ArrayAdapter<Integer> courseNumberArrayAdapter = new ArrayAdapter<Integer>(ChatRoomCreateActivity.this,
                                 android.R.layout.simple_spinner_item, response.body());
-                        course.setAdapter(courseNumberArrayAdapter);
+                        courseSpinner.setAdapter(courseNumberArrayAdapter);
                     }
 
                     @Override
@@ -67,10 +70,26 @@ public class ChatRoomCreateActivity extends AppCompatActivity {
 
         submit = (Button) findViewById(R.id.submit);
         cancel = (Button) findViewById(R.id.cancel);
+
+        FormValidator chatFV = new FormValidator(chatroomName, "Chatroom Name");
+
+        chatroomName.addTextChangedListener(chatFV);
+        chatroomName.setOnFocusChangeListener(chatFV);
     }
 
     public void submit(View view) {
-
+        String name = chatroomName.getText().toString();
+        Course course = new Course((CourseType) courseType.getSelectedItem(),
+                (Integer) courseSpinner.getSelectedItem());
+        boolean isValid = chatroomName.getError() == null && !TextUtils.isEmpty(name);
+        if (isValid) {
+            //go into new chatroom
+            //make the chatroom have a list of new users
+            Intent intent = new Intent(this, ActiveChatRoomActivity.class);
+            startActivity(intent);
+        } else {
+            Toast.makeText(this, "Information entered not valid", Toast.LENGTH_SHORT).show();
+        }
     }
     public void cancel(View view) {
         Intent intent = new Intent(this, ChatRoomListActivity.class);
